fda61aae923748c59c11cef8d4328c3ac22c8809
20181102932 <908907990@qq.com>
2019-09-18 11:55:02 +0800

    Update main.swift

--- swift exercise1/main.swift
+++ swift exercise1/main.swift
@@ -1,4 +1,4 @@
-var a = 0;
+/*var a = 0;
 var sum = 0;
 while a < 100
 {
@@ -13,3 +13,50 @@ while a < 100
     }
 }
 print(sum)
+
+var shuzu:[Int] = [1,2,3,4,5]
+for item in "wjxzhendiniupi"{
+    print(item)
+}
+var varA = 20
+let constA = 100
+var varC:Float = 20.0
+var stringA = "\(varA) 乘于 \(constA) 等于 \(varC * 100)"
+print(stringA)
+
+var array:[Int]=[2,4,5,10,46,13,6,7,9]
+var big = 0
+for item in array{
+    if (big < item)
+    {
+        big = item
+    }
+  
+}
+print(big)
+
+var array = [Int]()
+array.append(10)
+array.append(100)
+array += [204]
+for item in array{
+    print(item)
+}
+*/
+var strs = ["3","a","8","haha","100","hello"]
+var string = "a"
+var count1 = strs.count - 1
+var count2 = 0
+for item in strs{
+    while(count2 < count1)
+    {
+        string = strs[count2]
+        strs[count2] = strs[count1]
+        strs[count1] = string
+        count1 = count1 - 1
+        count2 = count2 + 1
+    }
+}
+for item in strs{
+    print(item)
+}
