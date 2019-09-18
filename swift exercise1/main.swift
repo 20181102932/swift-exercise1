0cd74d7c06330ded7215772cb24d3d74f05ffc30
20181102932 <908907990@qq.com>
2019-09-18 10:29:23 +0800

    Initial Commit

---
+++ swift exercise1/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  swift exercise1
+//
+//  Created by s20181102932 on 2019/9/18.
+//  Copyright © 2019 wjx. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
